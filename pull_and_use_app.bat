c4baf992c6c96c15222d4aa558947d42c1c0fd69
RonanRibouletPython <newsletterpython13@gmail.com>
2024-09-24 23:25:03 +0200

    bat file to pull docker image from docker hub and run the app on the pc

---
+++ pull_and_use_app.bat
@@ -0,0 +1,14 @@
+:: set the variables
+set IMAGE_NAME=my-fastapi-app
+set TAG=latest
+set USERNAME=ronanribouletpython
+set APP_URL=http://localhost:8000
+
+:: pull the docker image from the docker hub
+docker pull %USERNAME%/%IMAGE_NAME%
+
+:: run the pulled docker image
+docker run -d -p 8000:8000 %IMAGE_NAME%:%TAG%
+
+:: open the browser
+start "" %APP_URL%
