8d6509be7ff498046c25c3095248ed684cbaf967
RonanRibouletPython <newsletterpython13@gmail.com>
2024-09-30 21:33:20 +0200

    delete and update users

--- app/routes/user.py
+++ app/routes/user.py
@@ -5,7 +5,7 @@ from typing import List
 from sqlalchemy.orm import Session
 from app.utils.utility import get_db
 from app.schemas.user import UserCreate, UserBase
-from app.utils.user import create_user, get_user
+from app.utils.user import create_user, get_user, delete_user, update_user
 from app.db.models.user import User as UserModel
 from app.schemas.user import User as UserSchema
 
@@ -33,5 +33,21 @@ async def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
             status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
         )
     return user
+
+@router.delete("/{user_id}")
+async def delete_user_by_id(user_id: int, db: Session = Depends(get_db)):
+    """Delete a user by their ID."""
+    delete_user(db=db, user_id=user_id)
+    
+@router.put("/{user_id}", response_model=UserSchema)
+async def update_user_by_id(user_id: int, user: UserBase, db: Session = Depends(get_db)):
+    """Update a user by their ID."""
+    updated_user = update_user(db=db, user_id=user_id, user=user)
+    if not updated_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
+    return updated_user
+    
 
 
--- app/utils/user.py
+++ app/utils/user.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from app.db.models.user import User as UserModel  # Import the SQLAlchemy model 
 from app.schemas.user import User, UserBase, UserCreate
+from datetime import datetime
 
 def get_user(db: Session, user_id: int):
     """Get a user by their ID."""
@@ -21,6 +22,22 @@ def create_user(db: Session, user: UserCreate):
     """Create a new user."""
     db_user = UserModel(**user.model_dump())
     db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def delete_user(db: Session, user_id: int):
+    """Delete a user by their ID."""
+    db.query(UserModel).filter(UserModel.id == user_id).delete()
+    db.commit()
+
+def update_user(db: Session, user_id: int, user: UserBase):
+    """Update a user by their ID."""
+    db_user = db.query(UserModel).filter(UserModel.id == user_id).first()
+    db_user.name = user.name
+    db_user.age = user.age
+    db_user.partner_id = user.partner_id
+    db_user.updated_at = datetime.now()
     db.commit()
     db.refresh(db_user)
     return db_user
