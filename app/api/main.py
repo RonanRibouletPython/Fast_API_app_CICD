d6b6ae7bb72cf55ce222edf9b56bc263817e54bc
RonanRibouletPython <newsletterpython13@gmail.com>
2024-10-14 21:02:38 +0200

    html frontend

--- app/api/main.py
+++ app/api/main.py
@@ -1,34 +1,36 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.routes import activity, user, partner_request
 from app.db.db_setup import engine_postgres, Base
-from app.db.models.user import User
-from app.db.models.partner_request import PartnerRequest
-from app.db.models.activity import Activity
+from fastapi.middleware.cors import CORSMiddleware
 
-# commented because the database is created with alembic
-Base.metadata.create_all(bind=engine_postgres)
-
-description = """
-## Date Ideas
-
-This is the API for couples to share date ideas and plan them.
-
-"""
-
+# Initialize the FastAPI app
 app = FastAPI(
     title="Date Ideas Application",
-    description=description,
-    summary="Application for my Cricri and I <3",
+    description="This is the API for couples to share date ideas and plan them.",
     version="0.0.1",
     contact={
         "name": "Riboulet Ronan",
         "email": "ronanriboulet@gmail.com",
     },
-    )
+)
 
+# Middleware for CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to your frontend URL in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Serve static files
+app.mount("/static", StaticFiles(directory="app/frontend"), name="static")
+
+# Uncomment if you're creating tables with Alembic; comment if using Alembic migrations
+Base.metadata.create_all(bind=engine_postgres)
+
+# Include routers
 app.include_router(activity.router)
 app.include_router(user.router)
 app.include_router(partner_request.router)
-
-
-
